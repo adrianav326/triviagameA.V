f0552bd427784a152df15d35a1ac98ecf5488e14
Villela <0262659@students.dadeschools.net>
2019-04-16 12:01:22 -0400

    did stuff

---
+++ main.js
@@ -0,0 +1,21 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+//Question Set
+var questions = [
+
+    {
+        question: ""
+        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story",
+        image: 
+    },
+    {
+        question: "Which of these is NOT a name of the Spice Girls?"
+        answers: ["Sporty Spice", "Hot Spice", "Scary Spice", "Posh Spice"];
+        correctAnswer: ""
+        image: 
+    }
+
+]
