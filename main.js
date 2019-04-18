75723d527560e61de83f0064ea6079e743d76b58
Villela <0262659@students.dadeschools.net>
2019-04-18 11:52:20 -0400

    added to javascript and personalized questions

--- main.js
+++ main.js
@@ -6,16 +6,59 @@ var countStartNumber = 30;
 var questions = [
 
     {
-        question: ""
-        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
-        correctAnswer: "Toy Story",
+        question: "In the first movie of Harry Potter, which house did Harry choose?"
+        answers: ["Hufflepuff", "Slytherin", "Ravenvclaw", "Gryffindor"],
+        correctAnswer: "Gryffindor",
         image: 
     },
+
     {
-        question: "Which of these is NOT a name of the Spice Girls?"
-        answers: ["Sporty Spice", "Hot Spice", "Scary Spice", "Posh Spice"];
-        correctAnswer: ""
+        question: "Who was the voice of Woody in the movie Toy Story?"
+        answers: ["Tom Hanks", "Tim Allen", "John Morris", "Annie Potts"],
+        correctAnswer: "Tom Hanks"
         image: 
+    },
+
+] 
+
+//Variable to hold out setInterval
+var timer; 
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+            console.log("STOP");
+            game.timeUp();
+        }
+    },
+
+    loadQuestion: function() {
+        timer = setIneterval(game.countdown, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+        for (var i = 0; i < questions[this.currentQuestion].answers.length, i++);
+        {
+            panel.append("<button class= 'answer-button' id='button' data-name=''" + questions[this.currentQuestion].answers[i] + 
+            "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
+    nextQuestion: function() {
+        game.counter = countStartNumber;
+        $("#counter-number").html (game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+
+    timeUp: function(){
+
     }
 
-]
+}
